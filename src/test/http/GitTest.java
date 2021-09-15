f978e4192f01b1ee22b1788d5732117fe5a5183b
soeunnnn <90734058+soeunnnn@users.noreply.github.com>
2021-09-15 14:53:02 +0900

    Update GitTest.java
    원격리포지토리 변경 테스트

--- src/test/http/GitTest.java
+++ src/test/http/GitTest.java
@@ -4,6 +4,6 @@ public class GitTest {
 	
 	private static final String DESC = "GIT TEST용 클래스 입니다.";
 	private static final String CREDENTIAL = "GIT 인증 테스트";
-	private static final String GITHUB = "원격리포지토리에서 직접 수정"
+	private static final String GITHUB = "원격리포지토리에서 직접 수정";
 
 }
