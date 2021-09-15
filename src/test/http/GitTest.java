e7a4054146bc65dacf5cb0185325714b6dde5300
soeun <thdms12333@gmail.com>
2021-09-15 15:01:57 +0900

    [소은] conflict test

--- src/test/http/GitTest.java
+++ src/test/http/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
 	private static final String DESC = "GIT TEST용 클래스 입니다.";
 	private static final String CREDENTIAL = "GIT 인증 테스트";
 	private static final String GITHUB = "원격리포지토리에서 직접 수정";
+	private static final String LOCALCONFICT = "local에서 Conflict 테스트";
 	private static final String DEV = "DEV 브랜치에서 작업";
 
 }
